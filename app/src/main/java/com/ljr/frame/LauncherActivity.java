3a8caddfb236a611c745cdade5898042519be527
ljtRookie <15622732935@163.com>
2017-04-08 11:11:56 +0800

    启动页

--- app/src/main/java/com/ljr/frame/LauncherActivity.java
+++ app/src/main/java/com/ljr/frame/LauncherActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TextView;
+
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -11,6 +14,12 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        TextView time = (TextView) findViewById(R.id.time);
+        Time t=new Time();
+        t.setToNow();
+        int month=t.month+1;
+        int monthDay=t.monthDay+1;
+        time.setText(t.year+":"+month+":"+monthDay);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
